a7f5e53ac52930839120e88830dba139c17ab25b
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-03-17 13:51:38 +0100

    Setting up background images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 
+import hero_mobile from "./images/image-hero-mobile.jpg";
+import hero_desktop from "./images/image-hero-desktop.jpg";
+
 function App() {
-  return <h1>Hello FM#17</h1>;
+  return (
+    <Home>
+      <Header></Header>
+      <main></main>
+    </Home>
+  );
 }
+
+const Home = styled.div`
+  min-height: 100vh;
+`;
+const Header = styled.header`
+  height: 20rem;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  @media (max-width: 375px) {
+    background-image: url(${hero_mobile});
+  }
+  @media (min-width: 376px) {
+    background-image: url(${hero_desktop});
+  }
+`;
 
 export default App;
